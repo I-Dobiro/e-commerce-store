9a816a1d01165ce490a9e15eb529e4d29e475c2d
Izk <97883727+I-Dobiro@users.noreply.github.com>
2026-02-25 18:14:11 +0000

    update

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "drizzle-kit";
-import { ENV } from "./src/config/env.js";
+import { ENV } from "./src/config/env";
 
 export default defineConfig({
     schema: "./src/db/schema.ts",
