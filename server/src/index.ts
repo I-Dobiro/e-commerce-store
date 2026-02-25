f84124ab6c2fd7885381577177d68d4d53b78ef7
Izk <97883727+I-Dobiro@users.noreply.github.com>
2026-02-25 14:23:37 +0000

    project updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
 import commentRoutes from './routes/commentRoutes';
+import path from 'path';
 
 
 const app = express();
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(clerkMiddleware()); // auth object will be available on req.auth
 app.use(express.urlencoded({ extended: true })); // parses data from HTML forms
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.json({
     message: "Welcome to The E-Commerce-Store - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
     endpoints: {
@@ -28,6 +29,14 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes); // user routes
 app.use("/api/products", productRoutes); // product routes
 app.use("/api/comments", commentRoutes); // comment routes
+
+if(ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
 
 app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
